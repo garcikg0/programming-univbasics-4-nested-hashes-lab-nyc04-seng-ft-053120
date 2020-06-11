f81eb4c20c197ee6b75ee074233bb56febdd31e6
Kevin Garcia <kg.gar91@gmail.com>
2020-06-11 06:34:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,12 +65,12 @@ def adding_matz
 #   :languages => ["LISP", "C"]
 # }
 # return the entire updated hash
-  programmer_hash[:yukihiro_matsumoto] => {
-  :known_for => "Ruby",
-  :languages => ["LISP", "C"]
-  }
 	programmer_hash =
  		{
+      :yukihiro_matsumoto => { 
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+      }
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
