b9597f54234c07705896d89a5b3557bad364bba0
Kevin Garcia <kg.gar91@gmail.com>
2020-06-11 06:17:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-  programmer_hash[:dennis_ritchies][:languages][1]
+  programmer_hash[:dennis_ritchies][:languages][ ]
 end
 
 def adding_matz
