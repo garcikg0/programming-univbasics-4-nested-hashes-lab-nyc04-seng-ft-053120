db1bec39e818d4bd75f1570df7302fcc67131974
Kevin Garcia <kg.gar91@gmail.com>
2020-06-11 06:32:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,10 +65,10 @@ def adding_matz
 #   :languages => ["LISP", "C"]
 # }
 # return the entire updated hash
-programmer_hash[:yukihiro_matsumoto] => {
+  programmer_hash[:yukihiro_matsumoto] => {
   :known_for => "Ruby",
   :languages => ["LISP", "C"]
-}
+  }
 	programmer_hash =
  		{
       :grace_hopper => {
